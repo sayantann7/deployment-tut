d49be8ddb1502aa4eefde56603057ab30694aaac
Sayantan Nandi <sayantannandi13@gmail.com>
2025-07-06 19:13:07 +0530

    Added http server

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import { prisma } from "@repo/db/client";
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+// @ts-ignore
+app.post('/signup', async (req, res) => {
+    const { username, password } = req.body;
+    if (!username || !password) {
+        return res.status(400).json({ error: 'Username and password are required' });
+    }
+    const user = await prisma.user.create({
+        data: {
+            username,
+            password,
+        },
+    });
+    return res.status(201).json(user);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
